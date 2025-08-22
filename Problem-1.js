cfbc1d0489294880e7280f39e1d1858490c60e4c
Vasantha62 <125265788+Vasantha62@users.noreply.github.com>
2025-08-22 10:43:27 +0530

    Add files via upload

---
+++ Problem-1.js
@@ -0,0 +1,27 @@
+// Problem-1.js
+
+class Calculator {
+  constructor(a, b, operation) {
+    this.a = a;
+    this.b = b;
+    this.operation = operation.toLowerCase();
+  }
+
+  calculate() {
+    switch (this.operation) {
+      case "add":
+        return this.a + this.b;
+      case "subtract":
+        return this.a - this.b;
+      case "multiply":
+        return this.a * this.b;
+      case "divide":
+        return this.b !== 0 ? this.a / this.b : "Error: Division by zero";
+      default:
+        return "Invalid operation";
+    }
+  }
+}
+
+const calc = new Calculator(10, 5, "add");
+console.log(calc.calculate()); // 15
